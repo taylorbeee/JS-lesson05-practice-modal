95336b1f9baf528dc15db91e9914d896958c6b97
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-12 11:33:04 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var span = document.querySelector(".modal-x");
+
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!";
+});
+span.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+});
